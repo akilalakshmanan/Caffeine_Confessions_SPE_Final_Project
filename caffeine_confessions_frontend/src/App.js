5352a6680de8d9bbf5cc8144b9b1a31502b2a8b4
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-04-25 16:21:57 +0530

    Front end code

---
+++ caffeine_confessions_frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Caffeine Confessions!!! </a>
+      </header>
+      <main>List of Coffee</main>
+    </div>
+  );
+}
+
+export default App;
