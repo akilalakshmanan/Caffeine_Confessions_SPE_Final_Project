8a528ff2e4664401cd1881a8e6380ec392d43374
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-04-25 19:33:32 +0530

    list of products added

--- caffeine_confessions_frontend/src/App.js
+++ caffeine_confessions_frontend/src/App.js
@@ -1,12 +1,40 @@
+import React from 'react';
+import data from './data';
+import logo from './logo.jpeg';
+
 function App() {
   return (
     <div>
+      <Header />
       <header>
         <a href="/">Caffeine Confessions!!! </a>
       </header>
-      <main>List of Coffee</main>
+      <main>
+        <h1>Featured products</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div className="product" key={products.slug}>
+              <a href={`/product/${products.slug}`}>
+                <img src={products.image} alt={products.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${products.slug}`}>
+                  <p>{products.name}</p>
+                </a>
+                <p>
+                  <strong>${products.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
 
+function Header() {
+  return <img src={logo} width="220px" height="180px" alt="Logo" />;
+}
 export default App;
