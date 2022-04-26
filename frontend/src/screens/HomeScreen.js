b46f7a2f91ce89a1e6852bc8a243985fce0da7d4
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-04-26 13:41:39 +0530

    Manage state by reducer hook

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,41 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-//import axios from 'axios';
+import { useEffect, useReducer } from 'react';
+import logger from 'use-reducer-logger';
+import axios from 'axios';
 //import data from '../data';
 
-async function callAction() {
-  let result;
-  await fetch('http://localhost:5000/api/products')
-    .then((res) => {
-      console.log('res', res);
-      return res.json();
-    })
-    .then((data) => {
-      result = data;
-      console.log('data', data);
-    });
-  console.log('result', result);
-  return result;
-}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  //const [products, setProducts] = useState([]);
   // returns an array that contains a variable products and a function setProducts
   // to update the variable product.
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: '',
+  });
   useEffect(() => {
     const fetchData = async () => {
-      const result = await callAction(); //ajax request sent to this address and put the result in result variable
-      setProducts(result);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const instance = axios.create({ baseURL: 'http://localhost:5000' });
+        const result = await instance.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
     };
     fetchData();
   }, []);
@@ -33,22 +43,38 @@ function HomeScreen() {
     <div>
       <h1>Featured products</h1>
       <div className="products">
-        {products.map((products) => (
+        {/* {products.map((products) => (
           <div className="product" key={products.slug}>
             <Link to={`/product/${products.slug}`}>
               <img src={products.image} alt={products.name} />
             </Link>
-            <div className="product-info">
+            <div className="product-info"> */}
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((products) => (
+            <div className="product" key={products.slug}>
               <Link to={`/product/${products.slug}`}>
-                <p>{products.name}</p>
+                <img src={products.image} alt={products.name} />
               </Link>
-              <p>
+              {/* <p>
                 <strong>${products.price}</strong>
               </p>
-              <button>Add to Cart</button>
+              <button>Add to Cart</button> */}
+              <div className="product-info">
+                <Link to={`/product/${products.slug}`}>
+                  <p>{products.name}</p>
+                </Link>
+                <p>
+                  <strong>${products.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
