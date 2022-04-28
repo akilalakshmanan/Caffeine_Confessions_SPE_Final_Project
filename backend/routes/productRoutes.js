3209146eb2877ccd4ac131351ebc6ceee8ecfd5a
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-04-28 14:58:13 +0530

    Dockerfile added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,34 +4,34 @@ import expressAsyncHandler from 'express-async-handler';
 
 const productRouter = express.Router();
 
-productRouter.get('/',async (req,res) => {
-    console.log('came here 1')
-    const products = await Product.find();
-    console.log('came here 2')
-    res.send(products);
-})
+productRouter.get('/', async (req, res) => {
+  console.log('came here 1');
+  const products = await Product.find();
+  console.log('came here 2');
+  res.send(products);
+});
 
 productRouter.post(
-    '/',
-    expressAsyncHandler( async(res,req)=>{
-        const newProduct = new Product({
-            name: 'sample name ' + Date.now(),
-            slug: 'sample-name-' + Date.now(),
-            image: '/images/p2.jpg',
-            price: 0,
-            category: 'sample category',
-            brand: 'sample brand',
-            countInStock: 0,
-            rating: 0,
-            numReviews: 0,
-            description: 'sample description',
-        });
-        const product = await newProduct.save();
-        req.send({
-            message: "Product Created",
-            product
-        })
-    })
+  '/',
+  expressAsyncHandler(async (res, req) => {
+    const newProduct = new Product({
+      name: 'sample name ' + Date.now(),
+      slug: 'sample-name-' + Date.now(),
+      image: '/images/p3.jpg',
+      price: 0,
+      category: 'sample category',
+      brand: 'sample brand',
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: 'sample description',
+    });
+    const product = await newProduct.save();
+    req.send({
+      message: 'Product Created',
+      product,
+    });
+  })
 );
 
 export default productRouter;
