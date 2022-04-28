53deccfd6219433e0a325c5663c713e785c26e77
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-04-28 10:11:21 +0530

    adding extra screens and model changes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,18 +1,37 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import expressAsyncHandler from 'express-async-handler';
+import { isAuth, isAdmin } from '../utils.js';
 
 const productRouter = express.Router();
 
-productRouter.get('/',async (req,res) => {
-    console.log('came here 1')
-    const products = await Product.find();
-    console.log('came here 2')
-    res.send(products);
-})
+productRouter.get(
+    '/',
+    expressAsyncHandler(async (req,res) => {
+        const products = await Product.find();
+        res.send(products);
+    })
+);
+
+productRouter.get(
+    '/slug/:slug', 
+    expressAsyncHandler(async (req, res) => {
+        console.log('came here 1')
+        const product = await Product.findOne({ slug: req.params.slug });
+        console.log(product);
+        console.log('came here 2')
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).send({ message: 'Product Not Found' });
+        }
+    })
+);
 
 productRouter.post(
     '/',
+    isAuth,
+    isAdmin,
     expressAsyncHandler( async(res,req)=>{
         const newProduct = new Product({
             name: 'sample name ' + Date.now(),
@@ -22,8 +41,6 @@ productRouter.post(
             category: 'sample category',
             brand: 'sample brand',
             countInStock: 0,
-            rating: 0,
-            numReviews: 0,
             description: 'sample description',
         });
         const product = await newProduct.save();
@@ -33,5 +50,167 @@ productRouter.post(
         })
     })
 );
+
+productRouter.put(
+    '/:id',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const productId = req.params.id;
+      const product = await Product.findById(productId);
+      if (product) {
+        product.name = req.body.name;
+        product.slug = req.body.slug;
+        product.price = req.body.price;
+        product.image = req.body.image;
+        product.images = req.body.images;
+        product.category = req.body.category;
+        product.brand = req.body.brand;
+        product.countInStock = req.body.countInStock;
+        product.description = req.body.description;
+        await product.save();
+        res.send({ message: 'Product Updated' });
+      } else {
+        res.status(404).send({ message: 'Product Not Found' });
+      }
+    })
+  );
+
+productRouter.delete(
+    '/:id',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const product = await Product.findById(req.params.id);
+      if (product) {
+        await product.remove();
+        res.send({ message: 'Product Deleted' });
+      } else {
+        res.status(404).send({ message: 'Product Not Found' });
+      }
+    })
+  );
+
+const PAGE_SIZE = 3;
+productRouter.get(
+    '/admin',
+    isAuth,
+    isAdmin,
+    expressAsyncHandler(async (req, res) => {
+      const { query } = req;
+      const page = query.page || 1;
+      const pageSize = query.pageSize || PAGE_SIZE;
+  
+      const products = await Product.find()
+        .skip(pageSize * (page - 1))
+        .limit(pageSize);
+      const countProducts = await Product.countDocuments();
+      res.send({
+        products,
+        countProducts,
+        page,
+        pages: Math.ceil(countProducts / pageSize),
+      });
+    })
+  );
+
+  productRouter.get(
+    '/categories',
+    expressAsyncHandler(async (req, res) => {
+      const categories = await Product.find().distinct('category');
+      res.send(categories);
+    })
+  );
+
+  productRouter.get(
+    '/search',
+    expressAsyncHandler(async (req, res) => {
+        
+      const { query } = req;
+      const pageSize = query.pageSize || PAGE_SIZE;
+      const page = query.page || 1;
+      const category = query.category || '';
+      const price = query.price || '';
+      const rating = query.rating || '';
+      const order = query.order || '';
+      const searchQuery = query.query || '';
+  
+      const queryFilter =
+        searchQuery && searchQuery !== 'all'
+          ? {
+              name: {
+                $regex: searchQuery,
+                $options: 'i',
+              },
+            }
+          : {};
+      const categoryFilter = category && category !== 'all' ? { category } : {};
+      const ratingFilter =
+        rating && rating !== 'all'
+          ? {
+              rating: {
+                $gte: Number(rating),
+              },
+            }
+          : {};
+      const priceFilter =
+        price && price !== 'all'
+          ? {
+              // 1-50
+              price: {
+                $gte: Number(price.split('-')[0]),
+                $lte: Number(price.split('-')[1]),
+              },
+            }
+          : {};
+      const sortOrder =
+        order === 'featured'
+          ? { featured: -1 }
+          : order === 'lowest'
+          ? { price: 1 }
+          : order === 'highest'
+          ? { price: -1 }
+          : order === 'toprated'
+          ? { rating: -1 }
+          : order === 'newest'
+          ? { createdAt: -1 }
+          : { _id: -1 };
+  
+      const products = await Product.find({
+        ...queryFilter,
+        ...categoryFilter,
+        ...priceFilter,
+        ...ratingFilter,
+      })
+        .sort(sortOrder)
+        .skip(pageSize * (page - 1))
+        .limit(pageSize);
+  
+      const countProducts = await Product.countDocuments({
+        ...queryFilter,
+        ...categoryFilter,
+        ...priceFilter,
+        ...ratingFilter,
+      });
+      res.send({
+        products,
+        countProducts,
+        page,
+        pages: Math.ceil(countProducts / pageSize),
+      });
+    })
+  );
+
+productRouter.get('/:id', 
+    expressAsyncHandler(async (req, res) => {
+        console.log('search made',req.params.id);
+        const product = await Product.findById(req.params.id);
+        if (product) {
+        res.send(product);
+        } else {
+        res.status(404).send({ message: 'Product Not Found' });
+        }
+    })
+  );
 
 export default productRouter;
